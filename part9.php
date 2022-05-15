eab897e4abec164697f49b882d1d74216a74e458
Ian <ianwachira612@outlook.com>
2022-05-15 16:45:39 -0400

    postDetails array

---
+++ part9.php
@@ -0,0 +1,9 @@
+<?php
+    ini_set('display_errors', 1);   // show errors for debugging
+    $firstName = "Jane";
+    $lastName = "JDoe";
+    $age = "Jane";
+    $married = "Jane";
+    $childrenNames = array("Alice", "Bob");
+    $isSettled =
+?>
